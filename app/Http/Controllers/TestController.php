fa8beb2f0ca1f38b3f9d25be734fdb7036c6ff90
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2024-01-08 11:25:10 +0200

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,5 +9,9 @@ class TestController extends Controller
    
    public function index(Request $request){
     var_dump($request->name);
+    echo $this->user();
+   }
+   public function user () {
+    return 'asd';
    }
 }
