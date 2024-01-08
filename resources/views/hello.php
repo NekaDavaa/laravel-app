8fa826384690c6b0a8f7f080da390694bba650ba
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2024-01-08 10:42:24 +0200

    asd

---
+++ resources/views/hello.php
@@ -0,0 +1,1 @@
+<h1>Hello ларавел <?php echo $id ?></h1>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello/{id}', function ($id) {
+    return view('hello', ['id' => $id]);
+});
